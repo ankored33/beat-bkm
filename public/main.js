99d4c9e498032e7d032a53283cea94dcffd44f09
Masato Doi <bridgehead33@gmail.com>
2017-01-03 08:00:50 +0000

    音関係を整えた

--- public/main.js
+++ public/main.js
@@ -21,15 +21,17 @@ $(function() {
       if (beat == true) {
         var size = Number($('#bkm-remaining-figure').text());
         var i = $(this).find('.figure').text();
-        var voiceRand = Math.floor( Math.random() * 8 );
+        var voiceRand = Math.floor( Math.random() * 9 );
         var leftRand = Math.floor( Math.random() * 1200 );
         var rival
-          if ($(this).find('.bkm-user').text().slice(0,1).match(/[a-j]/) != null ) {
+          if ($(this).find('.bkm-user').text().slice(0,1).match(/[a-g]/) != null ) {
             rival = '00';
-          } else if ($(this).find('.bkm-user').text().slice(0,1).match(/[k-t]/) != null ) {
+          } else if ($(this).find('.bkm-user').text().slice(0,1).match(/[h-n]/) != null ) {
             rival = '01';
-          } else {
+          } else if ($(this).find('.bkm-user').text().slice(0,1).match(/[o-u]/) != null ) {
             rival = '02';
+          } else {
+            rival = '03';
           }
         console.log(rival);
         var safeRand = Math.floor( Math.random() * 4 );
@@ -37,8 +39,8 @@ $(function() {
         console.log(i);
         if (i == "") {
           $(this).css('z-index','100');
-          $('.btnsound0'+voiceRand).get(0).currentTime = 0;
-          $('.btnsound0'+voiceRand).get(0).play();
+          $('.sound0'+voiceRand).get(0).currentTime = 0;
+          $('.sound0'+voiceRand).get(0).play();
           $.when(
             $(this).animate({ 'bottom':'300px','right': leftRand },150)
           ).done(function() {
@@ -47,8 +49,8 @@ $(function() {
             $(this).attr('class', "bkm-box-dead");
           });
         } else if (Number(i) > 0) {
-          $('.rival-safe-'+ rival + '-0' + safeRand).get(0).currentTime = 0;
-          $('.rival-safe-'+ rival + '-0' + safeRand).get(0).play();
+          $('.rival'+ rival + '-safe0' + safeRand).get(0).currentTime = 0;
+          $('.rival'+ rival + '-safe0' + safeRand).get(0).play();
           $.when(
             $(this).animate({ 'left' : '15px' },50)
           ).done(function() {
@@ -57,8 +59,8 @@ $(function() {
           i = i - 1;
           $(this).find('.figure').text(i);
         } else if (Number(i) == 0) {
-          $('.rival-die-'+ rival).get(0).currentTime = 0;
-          $('.rival-die-'+ rival).get(0).play();
+          $('.rival' + rival + '-die').get(0).currentTime = 0;
+          $('.rival' + rival + '-die').get(0).play();
           $(this).fadeOut(1500);
           $(this).attr('class', 'bkm-box-dead');
           $('#bkm-remaining-figure').text(size - 1);
