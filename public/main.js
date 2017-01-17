9096c07d47fe6d68a1c3b5b4a22c4b273fa968bc
Masato Doi <bridgehead33@gmail.com>
2017-01-17 15:08:18 +0000

    音をシンプル化

--- public/main.js
+++ public/main.js
@@ -239,14 +239,12 @@ $(function(){
           $('#bkm-remaining-figure').text(size - 1);
           beatBkm(this);
         } else if (Number(i) > 100 ) {
-          playSound('rival'+ rival + '-safe');
-          $.when(
-            $(this).animate({ 'left' : '15px' },50)
-          ).done(function() {
-            $(this).animate({ 'left':'0px' },50);
-          });
-          i = i - 1;
-          $(this).attr('class', 'bkm-box-rival');
+          playSound('rival' + rival + '-die');
+          $(this).find('.bkm-comment').css('color', 'red');
+          $(this).find('.bkm-user').css('color', 'red');
+          $(this).fadeOut(1500);
+          $(this).attr('class', 'bkm-box-dead');
+          $('#bkm-remaining-figure').text(size - 1);
         }
       } else {
       }
@@ -273,49 +271,6 @@ $(function(){
 });
 
 
-
-/*殴る(rival)------------------------------------------------------------*/
-$(function(){
-    $('#main').on('click', '.bkm-box-rival', function(){
-      if (canBeat == true) {
-        size = Number($('#bkm-remaining-figure').text());
-        txt = $(this).find('.bkm-comment').text();
-        i = txt.length;
-        damageRand = Math.floor( Math.random() * 4 );
-          if ($(this).find('.bkm-user').text().slice(0,1).match(/[a-g]/) != null ) {
-            rival = '00';
-          } else if ($(this).find('.bkm-user').text().slice(0,1).match(/[h-n]/) != null ) {
-            rival = '01';
-          } else if ($(this).find('.bkm-user').text().slice(0,1).match(/[o-u]/) != null ) {
-            rival = '02';
-          } else {
-            rival = '03';
-          }
-        if (Number(i) >= 20 ) {
-          playSound('rival'+ rival + '-damage0' + damageRand);
-          $.when(
-            $(this).animate({ 'left' : '15px' },50)
-          ).done(function() {
-            $(this).animate({ 'left':'0px' },50);
-          });
-          i = i - 1;
-          $(this).find('.bkm-comment').text(txt.replace(/....................$/,""));
-        }
-        else if (Number(i) < 20) {
-          playSound('rival' + rival + '-die');
-          $(this).find('.bkm-comment').css('color', 'red');
-          $(this).find('.bkm-user').css('color', 'red');
-          $(this).fadeOut(1500);
-          $(this).attr('class', 'bkm-box-dead');
-          $('#bkm-remaining-figure').text(size - 1);
-        }
-      } else {
-        console.log('checkbox false');
-      }
-    });
-});
-
-
 /*SoundJS ------------------------------------------------------------*/
 function registerSound () {
   var manifest = 
@@ -329,29 +284,9 @@ function registerSound () {
       {src:'/sound/scream03.mp3', id:'scream03'},
       {src:'/sound/scream04.mp3', id:'scream04'},
       {src:'/sound/scream05.mp3', id:'scream05'},
-      {src:'/sound/rival/cool/damage00.mp3', id:'rival00-damage00'},
-      {src:'/sound/rival/cool/damage01.mp3', id:'rival00-damage01'},
-      {src:'/sound/rival/cool/damage02.mp3', id:'rival00-damage02'},
-      {src:'/sound/rival/cool/damage03.mp3', id:'rival00-damage03'},
-      {src:'/sound/rival/cool/safe.mp3', id:'rival00-safe'},
       {src:'/sound/rival/cool/die.mp3', id:'rival00-die'},
-      {src:'/sound/rival/hero/damage00.mp3', id:'rival01-damage00'},
-      {src:'/sound/rival/hero/damage01.mp3', id:'rival01-damage01'},
-      {src:'/sound/rival/hero/damage02.mp3', id:'rival01-damage02'},
-      {src:'/sound/rival/hero/damage03.mp3', id:'rival01-damage03'},
-      {src:'/sound/rival/hero/safe.mp3', id:'rival01-safe'},
       {src:'/sound/rival/hero/die.mp3', id:'rival01-die'},
-      {src:'/sound/rival/priest/damage00.mp3', id:'rival02-damage00'},
-      {src:'/sound/rival/priest/damage01.mp3', id:'rival02-damage01'},
-      {src:'/sound/rival/priest/damage02.mp3', id:'rival02-damage02'},
-      {src:'/sound/rival/priest/damage03.mp3', id:'rival02-damage03'},
-      {src:'/sound/rival/priest/safe.mp3', id:'rival02-safe'},
       {src:'/sound/rival/priest/die.mp3', id:'rival02-die'},
-      {src:'/sound/rival/witch/damage00.mp3', id:'rival03-damage00'},
-      {src:'/sound/rival/witch/damage01.mp3', id:'rival03-damage01'},
-      {src:'/sound/rival/witch/damage02.mp3', id:'rival03-damage02'},
-      {src:'/sound/rival/witch/damage03.mp3', id:'rival03-damage03'},
-      {src:'/sound/rival/witch/safe.mp3', id:'rival03-safe'},
       {src:'/sound/rival/witch/die.mp3', id:'rival03-die'}];
     createjs.Sound.registerSounds(manifest);
 }
